ff1b21a0fe3d424c1d3a32d68f3f24c7930c21b7
wangsongyan <1104237534@qq.com>
2015-11-28 13:56:01 +0800

    修改XinGe.js的编写方式

--- www/XinGe.js
+++ www/XinGe.js
@@ -1,27 +1,6 @@
-
-var cordova = require('cordova');
-
-var Tencent = function () {
-    //success 注册成功执行方法
-    //fail 注册失败执行方法
-    //推送注册
-    Tencent.prototype.registerPush = function (success, fail, userName) {
-        //'XinGe'对应我们在java文件中定义的类名
-        //registerPush对应我们在这个类中调用的自定义方法
-        //userName是我们客户端传递给这个方法的参数，是个string字段
-        cordova.exec(success, fail, 'XinGe', 'registerPush', [userName])
-    }
-    //推送反注册
-    Tencent.prototype.unregisterPush = function () {
-        cordova.exec(null, null, 'XinGe', 'unregisterPush', [])
-    }
-    
-  //获取Token
-    Tencent.prototype.getToken = function (success, fail) {
+module.exports = {
+    getToken: function (success, fail) {
         cordova.exec(success, fail, 'XinGe', 'getToken', [])
     }
-    
-}
-var tencent = new Tencent();
+};
 
-module.exports = tencent;
